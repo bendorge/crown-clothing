f49a8b9bc12ea43c0cb384edd34dd6cef54366d5
Ben Dorge <bendorge@gmail.com>
2019-11-12 19:59:24 -0600

    Implemented code to pull in shop data from firestore and stores it into the shop reducer from our shop page. The WithSpinner HOC was included

--- src/App.js
+++ src/App.js
@@ -13,8 +13,9 @@ import CheckoutPage from './pages/checkout/checkout.component';
 import Header from './components/header/header.component';
 
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+
 import { setCurrentUser } from './redux/user/user.actions';
-import { selectCurrentUser} from './redux/user/user.selectors'; 
+import { selectCurrentUser} from './redux/user/user.selectors';
 
 const HatsPage = () => (
   <div>
@@ -27,7 +28,7 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    const { setCurrentUser } = this.props;
+    const { setCurrentUser} = this.props;
     
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
@@ -43,7 +44,7 @@ class App extends React.Component {
         setCurrentUser(userAuth);
       }
     })
-  }
+  } 
 
   componentWillUnmount() {
     this.unsubscribeFromAuth();
